1c5e1a4d8069303c806cd0390c462e7206699485
tuan-binh <binh1892001@gmail.com>
2023-08-21 22:08:05 +0700

    upload

--- src/main/java/rikkei/academy/model/Address.java
+++ src/main/java/rikkei/academy/model/Address.java
@@ -3,13 +3,15 @@ package rikkei.academy.model;
 public class Address {
 	private int id;
 	private String address;
+	private boolean status;
 	
 	public Address() {
 	}
 	
-	public Address(int id, String address) {
+	public Address(int id, String address, boolean status) {
 		this.id = id;
 		this.address = address;
+		this.status = status;
 	}
 	
 	public int getId() {
@@ -26,5 +28,13 @@ public class Address {
 	
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	
+	public boolean isStatus() {
+		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
 }
--- src/main/java/rikkei/academy/reponsitory/impl/AddressDTO.java
+++ src/main/java/rikkei/academy/reponsitory/impl/AddressDTO.java
@@ -3,26 +3,135 @@ package rikkei.academy.reponsitory.impl;
 import rikkei.academy.model.Address;
 import rikkei.academy.reponsitory.IBaseDAO;
 
+import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class AddressDTO implements IBaseDAO<Address,Integer> {
+public class AddressDTO implements IBaseDAO<Address, Integer> {
+	
+	private DataSource dataSource;
+	private Connection con;
+	
 	@Override
 	public List<Address> findAll() {
-		return null;
+		List<Address> list = new ArrayList<>();
+		try {
+			con = dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			CallableStatement callSt = con.prepareCall("{call FIND_ALL_ADDRESS}");
+			ResultSet rs = callSt.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt("id_address");
+				String address = rs.getString("address_name");
+				boolean status = rs.getBoolean("status");
+				list.add(new Address(id, address, status));
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return list;
 	}
 	
 	@Override
 	public void save(Address address) {
-	
+		try {
+			con = dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			if (findById(address.getId()) == null) {
+				CallableStatement callSt = con.prepareCall("{call INSERT_ADDRESS(?,?)}");
+				callSt.setString(1, address.getAddress());
+				callSt.setBoolean(2, true);
+				callSt.executeUpdate();
+			} else {
+				CallableStatement callSt = con.prepareCall("{call UPDATE_ADDRESS(?,?,?)}");
+				callSt.setInt(1, address.getId());
+				callSt.setString(2, address.getAddress());
+				callSt.setBoolean(3, address.isStatus());
+				callSt.executeUpdate();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
 	}
 	
 	@Override
 	public void delete(Integer id) {
-	
+		try {
+			con = dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			CallableStatement callSt = con.prepareCall("{call DELETE_ADDRESS(?)}");
+			callSt.setInt(1, id);
+			callSt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
 	}
 	
 	@Override
 	public Address findById(Integer id) {
-		return null;
+		Address myAddress = null;
+		try {
+			con = dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			CallableStatement callSt = con.prepareCall("{call FINDBYID_ADDRESS(?)}");
+			callSt.setInt(1, id);
+			ResultSet rs = callSt.executeQuery();
+			while (rs.next()) {
+				String address = rs.getString("address_name");
+				boolean status = rs.getBoolean("status");
+				myAddress = new Address(id, address, status);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return myAddress;
 	}
 }
